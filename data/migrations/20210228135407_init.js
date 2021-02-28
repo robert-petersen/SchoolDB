30f454a242d2c5c9b51b12a2b72ef10a5a884685
Robert Petersen <robert.petersen808@gmail.com>
2021-02-28 14:09:14 -0600

    created migration

---
+++ data/migrations/20210228135407_init.js
@@ -0,0 +1,58 @@
+
+exports.up = function(knex) {
+  return knex.schema
+    .createTable("admins", tbl => {
+      tbl.increments("adminId");
+      tbl.string("username", 128).notNullable().unique();
+      tbl.string("password", 128).notNullable();
+    })
+    .createTable("volunteers", tbl => {
+      tbl.increments("volunteerId");
+      tbl.string("firstName", 128).notNullable();
+      tbl.string("lastName", 128).notNullable();
+      tbl.string("password", 128).notNullable();
+      tbl.string("subject", 128).notNullable();
+    })
+    .createTable("students", tbl => {
+      tbl.increments("studentId");
+      tbl.string("firstName", 128).notNullable();
+      tbl.string("lastName", 128).notNullable();
+      tbl.string("password", 128).notNullable();
+      tbl.boolean("needMeeting").notNullable();
+      tbl
+        .integer("volunteerId")
+        .unsigned()
+        .references("voulnteers.volunteerId")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    })
+    .createTable("tasks", tbl => {
+      tbl.increments("taskId");
+      tbl.string("task", 128).notNullable().unique();
+    })
+    .createTable("task-pairs", tbl => {
+      tbl.increments("taskPairId");
+      tbl
+        .integer("studentId")
+        .unsigned()
+        .references("students.studentId")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      tbl
+        .integer("taskId")
+        .unsigned()
+        .references("tasks.taskId")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      tbl.boolean("completed").defaultTo(false);
+    })
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTableIfExists("task-pairs")
+    .dropTableIfExists("tasks")
+    .dropTableIfExists("students")
+    .dropTableIfExists("volunteers")
+    .dropTableIfExists("admins");
+};
